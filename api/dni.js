bdabe9cc454fe90895d2405bba8036fee35f95a7
GaelVM <98351911+GaelVM@users.noreply.github.com>
2025-05-24 03:27:59 -0500

    ip

--- api/dni.js
+++ api/dni.js
@@ -8,13 +8,8 @@ module.exports = async (req, res) => {
     return res.status(400).json({ error: 'DNI es requerido' });
   }
 
-  try {
-    const response = await axios.get(`https://api.apis.net.pe/v2/reniec/dni?numero=${dni}`, {
-      headers: {
-        Authorization: `${process.env.API_RENIEC_TOKEN}`
-      }
-    });
-
+try {
+    const response = await axios.get(`https://dniruc.apisperu.com/api/v1/dni/${dni}?token=${process.env.API_PERU_TOKEN}`);
     res.status(200).json(response.data);
   } catch (error) {
     res.status(error.response?.status || 500).json({ error: error.message });
